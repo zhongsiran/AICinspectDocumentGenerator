298fb48fc8c25c6579f06782c12f133af5776a43
zhongsiran <35736240+zhongsiran@users.noreply.github.com>
2018-03-18 22:50:55 +0800

    dl_ins_rec function begin

--- DownloadInspectRecord.py
+++ DownloadInspectRecord.py
@@ -1,1 +1,19 @@
-import
+import requests
+
+class DownloadInspectRecord:
+
+    def __init__(self):
+        self.get_sp_list()
+
+    def get_sp_list(self):
+        params = {'secretkey': 'ZhongSiRan1990', 'div': 'SL', 'function': 'get_list'}
+        url = "https://shilingaic.applinzi.com/mylib/PyToolboxControllers/Sp_action_inspection_record_downloader.php"
+        list_page = requests.post(url , params)
+        try:
+            action_list = list_page.json()
+        except:
+            action_list = list_page.text
+        print(action_list)
+
+if __name__ == "__main__":
+    download = DownloadInspectRecord()
