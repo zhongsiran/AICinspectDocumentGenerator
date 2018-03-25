713d0efb680282f7bf32efc4f9b5c0602f5e2fd7
zhongsiran <35736240+zhongsiran@users.noreply.github.com>
2018-03-25 18:08:59 +0800

    修改SQL生成器为全局共用一个表的形式。

--- DownloadInspectRecord.py
+++ DownloadInspectRecord.py
@@ -1,19 +1,43 @@
 import requests
+import os
+import openpyxl
+
 
 class DownloadInspectRecord:
 
     def __init__(self):
         self.action_list = []
+        self.div = ''
+        self.division_list = ['SL', 'YH', 'TB']
+        self.choose_division()
         self.get_sp_list()
-        self.get_sp_record()
 
+    def choose_division(self):
+        while True:
+            print('请选择监管所')
+            print('1:狮岭')
+            print('2:裕华')
+            print('3:炭步')
+            division_index = input()
+            try:
+                division_index = int(division_index) - 1
+                self.div = self.division_list[division_index]
+                print(self.div)
+                break
+            except ValueError:
+                print('请输入对应的数字')
 
     def get_sp_list(self):
-        params = {'secretkey': 'ZhongSiRan1990', 'div': 'SL', 'function': 'get_list'}
+        params = {'secretkey': 'ZhongSiRan1990', 'div': self.div, 'function': 'get_list'}
         url = "https://shilingaic.applinzi.com/mylib/PyToolboxControllers/Sp_action_inspection_record_downloader.php"
-        list_page = requests.post(url , params)
-        try:
-            action_list = list_page.json()
+        list_page = requests.post(url, params)
+        #try:
+        action_list = list_page.json()
+        if action_list == '\u65e0\u4e13\u9879\u884c\u52a8\u6570\u636e':
+            print('该所无专项行动数据')
+            exit(0)
+            os.system('pause')
+        else:
             self.action_list = action_list
             print('请根据下列的列表选择你想下载的专项行动核查情况记录表。')
             action_no = 0
@@ -21,9 +45,10 @@ class DownloadInspectRecord:
                 for key in action:
                     print('第' + str(action_no + 1) + '号专项行动 - 代号:“' + key + '” -- 名称：“' + action[key] + '”')
                     action_no += 1
-        except:
-            result = list_page.text
-            print(result)
+            self.get_sp_record()
+        # except:
+        #     result = list_page.text
+        #     print('result:' + result)
 
     def get_sp_record(self):
         print('请输入行动序号（只需要数字），例如 1 ：')
@@ -43,14 +68,6 @@ class DownloadInspectRecord:
             except IndexError:
                 print('无此序号')
                 input_command = input()
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
