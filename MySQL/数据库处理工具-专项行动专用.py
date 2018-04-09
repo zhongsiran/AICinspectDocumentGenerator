29199c5c3c6e5dd1dd7891cb7391154f8407a310
hdslsgy <zsrxxh@163.com>
2018-04-09 08:43:07 +0800

    完善专项行动

--- MySQL/数据库处理工具-专项行动专用.py
+++ MySQL/数据库处理工具-专项行动专用.py
@@ -1,24 +1,28 @@
 #-*- coding: utf-8 -*-
-from openpyxl import load_workbook
-from openpyxl import Workbook
 from string import Template
-import os
+
+from openpyxl import load_workbook
+from openpyxl.worksheet import Worksheet
 
 
-class data:
+class Data:
     def __init__(self):
         self.datacontent = ''
         self.datatpl = Template("('${sano}','${n}','${no}','${pre_r}', '${pre_n}', '${c}','${r}','${sd}','${ed}'),\n")
+        self.div = ''
+        self.ws = Worksheet
+        self.head_tpl = ''
+        self.head = ''
 
-    def divselect(self):
-        self.headtpl = Template('''
+    def div_select(self):
+        self.head_tpl = Template('''
 insert into `hdscjg_database`.`${div}_zhuan_xiang_xing_dong`
 (`sp_action_no`,`sp_action_name`, `sp_action_daihao`, `sp_action_pre_regnum`, `sp_action_pre_name`, `sp_action_corpname`, `sp_action_regnum`, `sp_action_startdate`, `sp_action_enddate`) VALUES
 ''')
         self.div = input()
-        self.head = self.headtpl.substitute(div=self.div)
-        
-    def loadworkbook(self):
+        self.head = self.head_tpl.substitute(div=self.div)
+
+    def load_workbook(self):
         try:
             wb = load_workbook('专项行动记录表.xlsx')
             self.ws = wb.worksheets[0]
@@ -26,10 +30,9 @@ insert into `hdscjg_database`.`${div}_zhuan_xiang_xing_dong`
             print('当前目录没有“专项行动记录表.xlsx”文件')
             exit(0)
 
-        
-    def processtosql(self):
-        x=daihao=corpname=r=startdate=enddate=''
-        rows = self.ws[4:self.ws.max_row] #  第一行是标题，第二行是
+    def process_to_sql(self):
+
+        rows = self.ws[4:self.ws.max_row]  # 第一、二行是说明，第三行是标题
         for row in rows:
             try:
                 r = ''.join(row[4].value.split())
@@ -85,10 +88,10 @@ insert into `hdscjg_database`.`${div}_zhuan_xiang_xing_dong`
 
 
 if __name__ == '__main__':
-    data = data()
-    data.divselect()
-    data.loadworkbook()
-    data.processtosql()
+    data = Data()
+    data.div_select()
+    data.load_workbook()
+    data.process_to_sql()
     data.savefile() 
 
 
